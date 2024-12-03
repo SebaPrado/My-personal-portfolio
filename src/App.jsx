7ea6fe7a5c63e4ad51e99ed1e16dd957395a0bcd
SebaPrado <sebastian,pradomelesi@gmail.com>
2024-12-03 20:04:41 +0100

    7

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import About from "./Components/views/About";
 import NotFound from "./Components/views/NotFound";
 import Home0 from "./Components/views/Home0";
 import Home1 from "./Components/views/Home1";
- import FramerMotion from "./Components/views/FramerMotion"
- import FramerMotion2 from "./Components/views/FramerMotion2"
+import FramerMotion from "./Components/views/FramerMotion";
+import FramerMotion2 from "./Components/views/FramerMotion2";
 
- import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim"; 
+import { loadSlim } from "@tsparticles/slim";
 
 const router = createBrowserRouter([
   {
@@ -20,13 +20,12 @@ const router = createBrowserRouter([
     children: [
       {
         path: "",
-        element: <FramerMotion/>,
+        element: <FramerMotion />,
       },
       {
         path: "about",
         element: <About />,
       },
-      // Agrega la nueva ruta para Home0
       {
         path: "home0",
         element: <Home0 />,
@@ -52,122 +51,119 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  
-    const [init, setInit] = useState(false);
-  
-    // this should be run only once per application lifetime
-    useEffect(() => {
-      initParticlesEngine(async (engine) => {
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadAll(engine);
-        //await loadFull(engine);
-        await loadSlim(engine);
-        //await loadBasic(engine);
-      }).then(() => {
-        setInit(true);
-      });
-    }, []);
-  
-    const particlesLoaded = (container) => {
-     
-    };
-  
-    const options = useMemo(
-      () => ({
-        background: {
-          color: {
-            value: "#023047",
-          },
-          opacity:"1"
-        },
-        fpsLimit: 110,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: false,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-          },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 100,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 2,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-            },
-            value: 110,
-          },
-          opacity: {
-            value: 0.3,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 5 },
-          },
-        },
-        detectRetina: true,
-      }),
-      [],
-    );
-  
-    if (init) {
-      return (
-        <>
-          <Particles
-            id="tsparticles"
-            particlesLoaded={particlesLoaded}
-            options={options}
-            style={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              width: 'contain',
-              height: '100%',
-              zIndex: -1
-            }}
-          />
-          <RouterProvider router={router} />
-        </>
-      );
-    }
+  const [init, setInit] = useState(false);
 
-    return null;
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {};
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#023047",
+        },
+        opacity: "1",
+      },
+      fpsLimit: 110,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: false,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 100,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 110,
+        },
+        opacity: {
+          value: 0.3,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <>
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "contain",
+            height: "100%",
+            zIndex: -1,
+          }}
+        />
+        <RouterProvider router={router} />
+      </>
+    );
+  }
+
+  return null;
 }
 
 export default App;
