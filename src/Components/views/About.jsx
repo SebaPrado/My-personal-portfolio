f899961600606344e0e314518ca6dcf275df9c03
SebaPrado <sebastian,pradomelesi@gmail.com>
2024-12-03 20:03:24 +0100

    6

--- src/Components/views/About.jsx
+++ src/Components/views/About.jsx
@@ -1,20 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
 var string = "sebastian";
 function arrayMasLargo(string) {
   var arrayOriginal = string.split("");
