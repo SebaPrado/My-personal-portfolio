9e54bbee7558ee1d6d1cc9c264f165e49c390de4
SebaPrado <sebastian.pradomelesi@gmail.com>
2024-12-19 21:28:56 +0100

    3

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 function Root() {
-  // useEffect(() => {
-  //   const mainContainer = document.querySelector('.main-container');
-    
-  //   const handleScroll = () => {
-  //     if (window.scrollY > 0) {
-  //       mainContainer.classList.add('scrolled');
-  //     } else {
-  //       mainContainer.classList.remove('scrolled');
-  //     }
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
+ 
 
   return (
     <StrictMode>
